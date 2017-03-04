7d2bc2c53898779b8d5a762ff3a41a77bda49fbf
Liju Robin George <liju.leelives@gmail.com>
2017-03-04 10:27:22 -0500

    initial proto commit

---
+++ app/js/src/script.js
@@ -0,0 +1,268 @@
+(function () {
+    function DataFetcher(urlFactory, delay) {
+        var self = this;
+
+        self.repeat = false;
+        self.delay = delay;
+        self.timer = null;
+        self.requestObj = null;
+
+        function getNext() {
+            self.requestObj = $.ajax({
+                    url: urlFactory()
+                }).done(function(response) {
+                    $(self).trigger("stateFetchingSuccess", {
+                        result: response
+                    });
+                }).fail(function(jqXHR, textStatus, errorThrown) {
+                    $(self).trigger("stateFetchingFailure", {
+                        error: textStatus
+                    });
+                }).always(function() {
+                    if (self.repeat && _.isNumber(self.delay)) {
+                        self.timer = setTimeout(getNext, self.delay);
+                    }
+                });
+        }
+
+        self.start = function(shouldRepeat) {
+            self.repeat = shouldRepeat;
+            getNext();
+        };
+
+        self.stop = function() {
+            self.repeat = false;
+            clearTimeout(self.timer);
+        };
+
+        self.repeatOnce = function() {
+            getNext();
+        };
+
+        self.setDelay = function(newDelay) {
+            this.delay = newDelay;
+        };
+    }
+
+    var $trafficStatusList = $("#mockTrafficStat"),
+        df2 = new DataFetcher(function() {
+            return "/traffic_status/frozen";
+        });
+	var tempData = [];
+    $(df2).on({
+        "stateFetchingSuccess": function(event, data) {
+			data.result.data.forEach(function(dataEntry) {
+				//console.log(dataEntry);
+                tempData.push(dataEntry);
+            });
+			createCharts(tempData);
+        },
+        "stateFetchingFailure": function(event, data) {
+			console.log("Loading...");
+            setTimeout(function() {
+                $trafficStatusList.html("");
+                df2.repeatOnce();
+            }, 1000);
+        }
+    });
+
+    df2.start();
+})();
+
+var width = 960,
+    height = 500;
+
+var color = d3.scale.category20();
+
+var force = d3.layout.force()
+	.gravity(.05)
+    .distance(200)
+    .charge(-100)
+    .size([width, height]);
+
+var svg = d3.select(".demoContainer").append("svg")
+    .attr("width", width)
+    .attr("height", height)
+	//.append('svg:g')
+		.call(d3.behavior.zoom().on("zoom", redraw))
+	.append('svg:g');
+
+function redraw() {
+  //console.log("here", d3.event.translate, d3.event.scale);
+  svg.attr("transform","translate(" + d3.event.translate + ")"+ " scale(" + d3.event.scale + ")");
+}
+	
+//implementing pinning
+var node_drag = d3.behavior.drag()
+        .on("dragstart", dragstart)
+        .on("drag", dragmove)
+        .on("dragend", dragend);
+    function dragstart(d, i) {
+		d3.event.sourceEvent.stopPropagation();
+        force.stop() // stops the force auto positioning before you start dragging
+    }
+    function dragmove(d, i) {
+        d.px += d3.event.dx;
+        d.py += d3.event.dy;
+        d.x += d3.event.dx;
+        d.y += d3.event.dy;
+    }
+    function dragend(d, i) {
+        d.fixed = true; // of course set the node to fixed so the force doesn't include the node in its auto positioning stuff
+        force.resume();
+    }
+    function releasenode(d) {
+        d.fixed = false; // of course set the node to fixed so the force doesn't include the node in its auto positioning stuff
+        //force.resume();
+    }	
+//Starting Code for building graph
+function createCharts(tdata) {
+	var links = tdata;
+	
+	var nodesByName = {};
+	var linkedByIndex = {};
+	
+	links.forEach(function(link) {
+		if(link.srcObj != null || link.destObj != null) {
+			link.source = nodeByName(link.srcObj,link.srcType);
+			link.target = nodeByName(link.destObj,link.destType);
+			linkedByIndex[link.srcObj + "," + link.destObj] = 1;
+		}
+	});
+	
+	function neighboring(a, b){
+		return linkedByIndex[a.name + "," + b.name] || linkedByIndex[b.name + "," + a.name];
+	}
+	
+	var nodes = d3.values(nodesByName);
+	
+	//Creating a hashmap helper
+	function nodeByName(name, group) {
+		return nodesByName[name] || (nodesByName[name] = {name: name, group: group});
+	}
+	
+	// Per-type markers, as they don't inherit styles.
+	var marker = svg.append("defs").selectAll("marker")
+				.data(["end"])
+			  .enter().append("marker")
+				.attr("id", function(d) { return d; })
+				.attr("viewBox", "0 -5 10 10")
+				.attr("refX", 25)
+				.attr("refY", 0)
+				.attr("markerWidth", 6)
+				.attr("markerHeight", 6)
+				.attr("orient", "auto")
+			  .append("path")
+				.attr("d", "M0,-5L10,0L0,5 L10,0 L0, -5")
+				.style("stroke", "#4679BD")
+				.style("opacity", "0.6");
+	
+	// Create the link lines.
+	var link = svg.selectAll(".link")
+			.data(links)
+		.enter().append("line")
+			.attr("class", "link")
+			.attr("stroke", "#ddd")
+			.attr("stroke-opacity", 0.8)
+			.attr("marker-end", "url(#end)");
+
+	// Create the node circles.
+	var node = svg.selectAll(".node")
+      .data(nodes)
+    .enter().append("circle")
+      .attr("class", "node")
+      .attr("r", 10)
+	  .style("fill", function(d) { return color(d.group); })
+	  .style("stroke", "#ddd")
+	  .style("stroke-width", 1.0)
+      .on('click', releasenode)
+	  .call(node_drag);
+	  
+	node.append("title")
+      .text( function(d) { return d.name;});
+	  
+	node.on("mouseover", showDetails)
+        .on("mouseout", hideDetails)
+	  
+	// Start the force layout.
+	force
+      .nodes(nodes)
+      .links(links)
+      .on("tick", tick)
+      .start();
+
+	function tick() {
+		link.attr("x1", function(d) { return d.source.x; })
+			.attr("y1", function(d) { return d.source.y; })
+			.attr("x2", function(d) { return d.target.x; })
+			.attr("y2", function(d) { return d.target.y; });
+
+		node.attr("cx", function(d) { return d.x; })
+			.attr("cy", function(d) { return d.y; });
+	}
+	
+	//Mouseover tooltip function
+	function showDetails(d,i){
+		content = '<p class="">' + d.name + '</span></p>'
+		content += '<hr class="">'
+		content += '<p class="">' + d.name + '</span></p>'
+		//tooltip.showTooltip(content,d3.event)
+
+		//higlight connected links
+		link.attr("stroke", function(l) {
+			if (l.source == d || l.target == d){ 
+				return "red"; 
+			}
+			else { 
+				return "#ddd";
+			}
+		  })
+		  .attr("stroke-opacity", function(l){
+			if (l.source == d || l.target == d)
+				return 1.0; 
+			else 
+				return 0.5;
+		});
+		
+		//highlighting marker
+		marker.attr("stroke", function(l) {
+			return "red";
+		  })
+		  .attr("stroke-opacity", function(l){
+				return 1.0; 
+			
+		});
+		
+		//highlight neighboring nodes
+		node.style("stroke", function(n){
+			if (neighboring(d, n)){
+				return "red"; 
+			}
+			else 
+				return "#ddd";
+		})
+		.style("stroke-width", function(n) {
+			if (neighboring(d, n)) 
+				return 2.0; 
+			else 
+				return 1.0;
+		});
+				
+	  
+		//highlight the node being moused over
+		d3.select(this).style("stroke","black")
+			.style("stroke-width", 2.0);
+	}
+  
+	//Mouseout function
+	function hideDetails (d,i) {
+		node.style("stroke", function (n) {
+				return "#ddd";
+			})
+			.style("stroke-width", function(n){return 1.0});
+
+		link.attr("stroke", "#ddd")
+			.attr("stroke-opacity", 0.8);
+	}
+}
+
